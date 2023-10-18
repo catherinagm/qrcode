f22b02b327aee391b9e23116d03e6e02a5df8a20
Catherina Garcia <catherinagm@gmail.com>
2023-10-18 14:52:33 -0500

    Initial

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,10 @@
+import qrimage from "../assets/image-qr-code.png";
+
 function Card() {
   return (
     <section className="card-section flex items-center justify-center min-h-screen">
       <div className="card-container max-w-xs p-4 bg-white rounded-xl shadow-lg">
-        <img className="rounded-lg" src="src/assets/image-qr-code.png" />
+        <img className="rounded-lg" src={qrimage} alt="qrcode" />
         <div className="text-box p-0 p-4 ">
           <h3 className="xl:text-xl sm:text-base font-bold text-center mb-4">
             Improve your front-end skills by building projects
